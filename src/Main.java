46ef6209469fcbab4ec22b81780b4303c6c12c7e
TomWulf <Tom.Wulf@uc.edu>
2023-04-07 11:04:10 -0400

    Completed but not thoroughly tested FileslistMaker

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,95 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
 
 public class Main {
 
     private static Scanner in = new Scanner(System.in);
     private static ArrayList<String> lines = new ArrayList<>();
 
+    private static File curFile;
+    private static String curFileName;
+
+    private static boolean isDirty = false; // Indicates that the current memo has not been saved
+
+    // Use these
     public static void main(String[] args) {
 
-
-
-        String menuPrompt = "A – Add  D – Delete  P – Print  Q – Quit";
-        String cmd = ""; // A D P or Q
+        String menuPrompt = "O - Open   S - Save   A – Add   D – Delete  V – View  Q – Quit";
+        String cmd = ""; // O S A D V or Q
         boolean done = false;
 
+        try {
+            do {
+                showList();
+                cmd = SafeInput.getRegExString(in, menuPrompt, "[AaDdVvQqOoSs]");
+                cmd = cmd.toUpperCase();
+                switch (cmd) {
+                    case "A":
+                        // case "a":
+                        add();
+                        isDirty = true;
+                        break;
 
-        do {
-            showList();
-            cmd = SafeInput.getRegExString(in, menuPrompt, "[AaDdPpQq]");
-            cmd = cmd.toUpperCase();
-            switch (cmd)
-            {
-                case "A":
-                // case "a":
-                add();
-                break;
+                    case "D":
+                        //  case "d":
+                        delete();
+                        isDirty = true;
+                        break;
 
-                case "D":
-              //  case "d":
-                   delete();
-                    break;
+                    case "V":
+                        //  case "p":
+                        showList();
+                        break;
+                    case "O":
+                        if(isDirty) {
+                            System.out.println("You are about to loose the current memo data.");
+                            boolean saveYN = SafeInput.getYNConfirm(in, "Do you want to save the current memo?");
 
-                case "P":
-              //  case "p":
-                    showList();
-                    break;
+                            if (saveYN) {
+                                saveFile(curFileName);
+                            }
+                        }
+                        openFile();
+                        isDirty = false;
+                        break;
+                    case "S":
+                        if(!lines.isEmpty()) {
+                            if (curFile == null)
+                                curFileName = SafeInput.getNonZeroLengthString(in, "Enter the name for the file: ");
+                            else
+                                curFileName = curFile.getName();
 
-                case "Q":
-             //   case "q":
-                  quit();
-                    break;
-            }
+                            saveFile(curFileName);
+                            isDirty = false;
+                        }else
+                            System.out.println("Nothing to save!");
+                        break;
+                    case "Q":
+                        //   case "q":
+                        quit();
+                        break;
+                }
 
-        } while (!done);
+            } while (!done);
+
+         } // End of the Try block
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
     } // End of the Main method
 
     private static void showList()
@@ -67,17 +114,30 @@ public class Main {
      *  Quit function that checks for the user to confirm
      *
      */
-    private static void quit()
-    {
+    private static void quit() throws IOException {
         boolean quitYN = false;
+        boolean saveYN = false;
+        if(isDirty)
+        {
+            System.out.println("You are about to loose the data for your memo.");
+            System.out.println("Save the file before you Quit");
 
-        quitYN = SafeInput.getYNConfirm(in, "Are you sure");
+            saveYN = SafeInput.getYNConfirm(in, "Save the file? ");
+            if(saveYN)
+            {
+                saveFile(curFileName);
+                System.out.println("File Saved, exiting...");
+            }
+            System.exit(0);
+
+        }
+
+        quitYN = SafeInput.getYNConfirm(in, "Are you sure you want to Quit: ");
 
         if(quitYN)
         {
             System.exit(0);
         }
-
     }
 
     /**
@@ -130,6 +190,87 @@ public class Main {
 
     }
 
+    private static void openFile() throws IOException, FileNotFoundException
+    {
+        // Code to open the file goes here but not the try block since it uses the one in main.
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
 
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path wd = workingDirectory.toPath();
+        wd = Paths.get(workingDirectory + "\\src");
+        workingDirectory = wd.toFile();
+
+        // Typiacally, we want the user to pick the file so we use a file chooser
+        // kind of ugly code to make the chooser work with NIO.
+        // Because the chooser is part of Swing it should be thread safe.
+        chooser.setCurrentDirectory(workingDirectory);
+        // Using the chooser adds some complexity to the code.
+        // we have to code the complete program within the conditional return of
+        // the filechooser because the user can close it without picking a file
+
+        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            selectedFile = chooser.getSelectedFile();
+            curFile = selectedFile;
+            Path file = selectedFile.toPath();
+            // Typical java pattern of inherited classes
+            // we wrap a BufferedWriter around a lower level BufferedOutputStream
+            InputStream in =
+                    new BufferedInputStream(Files.newInputStream(file, CREATE));
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(in));
+
+            // Clear any existing list items when you open a file
+            lines.clear();
+
+            // Finally we can read the file LOL!
+            int line = 0;
+            while (reader.ready()) {
+                rec = reader.readLine();
+                line++;
+                lines.add(rec);
+                // echo to screen
+                System.out.printf("\nLine %4d %-60s ", line, rec);
+            }
+            reader.close(); // must close the file to seal it and flush buffer
+            System.out.println("\n\nData file read!");
+
+
+        } else  // User closed the chooser without selecting a file
+        {
+            System.out.println("No file selected!!! ... exiting.\nRun the program again and select a file.");
+        }
+    }
+
+    private static void saveFile(String fileName) throws IOException
+    {
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\" + fileName);
+
+        // Typical java pattern of inherited classes
+        // we wrap a BufferedWriter around a lower level BufferedOutputStream
+        OutputStream out =
+                new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+        BufferedWriter writer =
+                new BufferedWriter(new OutputStreamWriter(out));
+
+        if(curFile == null)
+            curFile = file.toFile();
+
+        // Finally can write the file LOL!
+
+        for(String rec : lines)
+        {
+            writer.write(rec, 0, rec.length());  // stupid syntax for write rec
+            // 0 is where to start (1st char) the write
+            // rec. length() is how many chars to write (all)
+            writer.newLine();  // adds the new line
+            System.out.println("Writing file data: " + rec);
+        }
+        writer.close(); // must close the file to seal it and flush buffer
+        System.out.println("Data file written: " + curFile.getName());
+
+    }
 
 } // End of the class NO Code After this!!!
